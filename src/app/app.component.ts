0f51f5c5e0f1d2bc5e60b0ac23f658fc485b0ee6
vikanshu-98 <vikanshu8954985003@gmail.com>
2022-05-31 15:48:50 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ThirdPractice';
+  title = 'Viknashu chahu';
+  value=2;
+
+  getValue(){
+    return "function chauhsn";
+  }
 }
